2c707fa03f170df66e90146d9120b0278295d448
yueyinqiu <lujiaqi2003@126.com>
2023-05-17 20:09:59 +0800

    dd

---
+++ LunarCsharpYiJingFrameworkExtensions/LunarCsharpYiJingFrameworkExtensions/EightCharExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Lunar.EightChar;
+using YiJingFramework.PrimitiveTypes;
+
+namespace LunarCsharpYiJingFrameworkExtensions;
+public static class EightCharExtensions
+{
+    public static Tiangan YearGan(this EightChar eightChar)
+    {
+        return new(eightChar.Lunar.YearGanIndexExact + 1);
+    }
+    public static Dizhi YearZhi(this EightChar eightChar)
+    {
+        return new(eightChar.Lunar.YearZhiIndexExact + 1);
+    }
+
+    public static Tiangan MonthGan(this EightChar eightChar)
+    {
+        return new(eightChar.Lunar.MonthGanIndexExact + 1);
+    }
+    public static Dizhi MonthZhi(this EightChar eightChar)
+    {
+        return new(eightChar.Lunar.MonthZhiIndexExact + 1);
+    }
+
+    public static Tiangan DayGan(this EightChar eightChar)
+    {
+        return new(eightChar.DayGanIndex + 1);
+    }
+    public static Dizhi DayZhi(this EightChar eightChar)
+    {
+        return new(eightChar.DayZhiIndex + 1);
+    }
+
+    public static Tiangan TimeGan(this EightChar eightChar)
+    {
+        return new(eightChar.Lunar.TimeGanIndex + 1);
+    }
+    public static Dizhi TimeZhi(this EightChar eightChar)
+    {
+        return new(eightChar.Lunar.TimeZhiIndex + 1);
+    }
+}
---
+++ LunarCsharpYiJingFrameworkExtensions/LunarCsharpYiJingFrameworkExtensions/LunarExtensions.cs
@@ -0,0 +1,88 @@
+﻿using LLunar = Lunar.Lunar;
+using YiJingFramework.PrimitiveTypes;
+
+namespace LunarCsharpYiJingFrameworkExtensions;
+public static class LunarExtensions
+{
+    public static Tiangan YearGan(this LLunar lunar)
+    {
+        return new(lunar.YearGanIndex + 1);
+    }
+    public static Dizhi YearZhi(this LLunar lunar)
+    {
+        return new(lunar.YearZhiIndex + 1);
+    }
+
+    public static Tiangan YearGanExact(this LLunar lunar)
+    {
+        return new(lunar.YearGanIndexExact + 1);
+    }
+    public static Dizhi YearZhiExact(this LLunar lunar)
+    {
+        return new(lunar.YearZhiIndexExact + 1);
+    }
+
+    public static Tiangan YearGanByLiChun(this LLunar lunar)
+    {
+        return new(lunar.YearGanIndexByLiChun + 1);
+    }
+    public static Dizhi YearZhiByLiChun(this LLunar lunar)
+    {
+        return new(lunar.YearZhiIndexByLiChun + 1);
+    }
+
+    public static Tiangan MonthGan(this LLunar lunar)
+    {
+        return new(lunar.MonthGanIndex + 1);
+    }
+    public static Dizhi MonthZhi(this LLunar lunar)
+    {
+        return new(lunar.MonthZhiIndex + 1);
+    }
+
+    public static Tiangan MonthGanExact(this LLunar lunar)
+    {
+        return new(lunar.MonthGanIndexExact + 1);
+    }
+    public static Dizhi MonthZhiExact(this LLunar lunar)
+    {
+        return new(lunar.MonthZhiIndexExact + 1);
+    }
+
+
+    public static Tiangan DayGan(this LLunar lunar)
+    {
+        return new(lunar.DayGanIndex + 1);
+    }
+    public static Dizhi DayZhi(this LLunar lunar)
+    {
+        return new(lunar.DayZhiIndex + 1);
+    }
+
+    public static Tiangan DayGanExact(this LLunar lunar)
+    {
+        return new(lunar.DayGanIndexExact + 1);
+    }
+    public static Dizhi DayZhiExact(this LLunar lunar)
+    {
+        return new(lunar.DayZhiIndexExact + 1);
+    }
+
+    public static Tiangan DayGanExact2(this LLunar lunar)
+    {
+        return new(lunar.DayGanIndexExact2 + 1);
+    }
+    public static Dizhi DayZhiExact2(this LLunar lunar)
+    {
+        return new(lunar.DayZhiIndexExact2 + 1);
+    }
+
+    public static Tiangan TimeGan(this LLunar lunar)
+    {
+        return new(lunar.TimeGanIndex + 1);
+    }
+    public static Dizhi TimeZhi(this LLunar lunar)
+    {
+        return new(lunar.TimeZhiIndex + 1);
+    }
+}
